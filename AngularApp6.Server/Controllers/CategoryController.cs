53361e8fa4bcd47c28ae06ee2ba108db124a395f
Ammar <amar.alomari@gmail.com>
2025-04-08 17:17:28 +0300

    Add project files.

---
+++ AngularApp6.Server/Controllers/CategoryController.cs
@@ -0,0 +1,64 @@
+﻿using AngularApp6.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularApp6.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+
+        public CategoryController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            var categories = _db.Categories.ToList();
+            return Ok(categories);
+        }
+
+
+        [HttpGet("getProductById")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _db.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [HttpPost ("getCategoryByName")]
+
+
+        public IActionResult GetCategoryByName(string name)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Name == name);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost ("getFirstCategory")]
+
+        public IActionResult GetFirstCategory()
+        {
+            var category = _db.Categories.FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+    }
+}
---
+++ AngularApp6.Server/Controllers/ProudctController.cs
@@ -0,0 +1,62 @@
+﻿using AngularApp6.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularApp6.Server.Controllers
+{
+    public class ProudctController : Controller
+    {
+        private readonly MyDbContext _db;
+
+        public ProudctController(MyDbContext db)
+        {
+            _db = db;
+        }
+  
+
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            var Product = _db.Products.ToList();
+            return Ok(Product);
+        }
+
+
+        [HttpGet("getProductById")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _db.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+        [HttpPost("getProductByName")]
+
+
+        public IActionResult GetProductByName(string name)
+        {
+            var product = _db.Products.FirstOrDefault(c => c.Name == name);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost("getFirstproduct")]
+
+        public IActionResult GetFirstProduct()
+        {
+            var Product = _db.Products.FirstOrDefault();
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return Ok(Product);
+        }
+    }
+}
